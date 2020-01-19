77920805c5df5253870ca8b48632ec38c7dceba3
Wagner Reis <wagnerreis1997@gmail.com>
2020-01-19 11:20:11 -0300

    Tela principal e componente Button

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,14 +1,39 @@
 import React from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+import Button from '../components/Button'
 
 const HomeScreen = props => {
     return (
-        <View>
-            <TouchableHighlight onPress={() => props.navigation.navigate('EMOM')}>
-                <Text>HomeScreen</Text>
-            </TouchableHighlight>
+        <View style={styles.container}>
+            <Text style={styles.logo} >CalisTimer</Text>
+            <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>EMOM</Button>
+            <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>AMRAP</Button>
+            <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>Isometria</Button>
         </View>
     )
 }
+HomeScreen.navigationOptions = {
+    headerShown: false
+}
+
+const styles = StyleSheet.create({
+    container: {
+         flex: 1, 
+         backgroundColor: '#D6304A' 
+    },
+
+    logo: {
+        fontFamily: 'Ubuntu-bold',
+        fontSize: 48,
+        textAlign: 'center',
+        color: 'white',
+        marginTop: 100, marginBottom: 100
+    },
+
+    btn: {
+        padding: 20
+    }
+})
 
 export default HomeScreen
